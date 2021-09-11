34de52a28ef753b0b6c68f5345dacc413dd80f4c
Ramiro quispe <ramiroquisper@gmail.com>
2021-09-11 09:04:45 -0500

    Add VehiclesType

---
+++ Vehicles03.API/Data/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vehicles03.API.Data.Entities;
+
+namespace Vehicles03.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<VehicleType> VehicleTypes { get; set; }
+
+        //Para que la descripcion de la tabla VehicleType dea unica ponemos lo siguiente
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<VehicleType>()
+                .HasIndex(x => x.Desription).IsUnique();
+        }
+    }
+}
---
+++ Vehicles03.API/Migrations/20210911135829_AddVehicleType.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vehicles03.API.Migrations
+{
+    public partial class AddVehicleType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VehicleTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Desription = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VehicleTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VehicleTypes_Desription",
+                table: "VehicleTypes",
+                column: "Desription",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VehicleTypes");
+        }
+    }
+}
